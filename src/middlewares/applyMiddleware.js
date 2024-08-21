9ae23f5b882dc9e9dd1b36b5940e20b3ea4a2c66
MuhammadRukon <muhammad.rukon242@gmail.com>
2024-08-21 23:13:12 +0600

    fix: dotenv

--- src/middlewares/applyMiddleware.js
+++ src/middlewares/applyMiddleware.js
@@ -4,14 +4,18 @@ const cors = require("cors");
 const applyMiddleware = (express, app) => {
   app.use(express.json());
   app.use(cookieParser());
+  require("dotenv").config();
+
   app.use(
     cors({
       origin: [process.env.LOCAL_CLIENT, process.env.REMOTE_CLIENT],
       // origin: ["http://localhost:5173"],
       credentials: true,
     })
+    
   );
-  require("dotenv").config();
+  
+  
 };
 
 module.exports = applyMiddleware;
